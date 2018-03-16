7fc04536adeefb8a3bfc72aa0ff5f26858d672dc
Kacper Dębowski <kdebowski93@gmail.com>
2018-03-16 15:15:37 +0100

    Zadanie domowe

--- lab2/zadanie.js
+++ lab2/zadanie.js
@@ -4,6 +4,34 @@
 var WatchModule = (function() {
     var watches = [];
 
+    _addWatch = function(w) {
+        watches.push(w);
+    }
+
+    _findWatchesByBrand = function(brand_name) {
+        return watches.filter(function(w) {
+            return w.brand.toLowerCase() === brand_name.toLowerCase();
+        });
+    }
+
+    _findWatchesByPrice = function(minPrice, maxPrice) {
+        return watches.filter(function(w) {
+            return w.price >= minPrice && w.price <= maxPrice;
+        })
+    }
+
+    _getMaleWatches = function() {
+        return watches.filter(function(w) {
+            return w.forMen;
+        });
+    }
+    
+    _getFemaleWatches = function() {
+        return watches.filter(function(w) {
+            return !w.forMen;
+        });
+    }
+
     return {
         Watch: function() {
             this.brand = '';
@@ -14,6 +42,48 @@ var WatchModule = (function() {
             this.waterResistance = 0;
         },
         addWatch: _addWatch,
-        findWatches: _findWatch,
+        findWatchesByBrand: _findWatchesByBrand,
+        findWatchesByPrice: _findWatchesByPrice,
+        getMaleWatches: _getMaleWatches,
+        getFemaleWatches: _getFemaleWatches,
     }
 })();
+
+var zeg1 = new WatchModule.Watch();
+zeg1.brand = "Fossil";
+zeg1.model = "JR1401P";
+zeg1.strapType = "Bransoleta";
+zeg1.price = 565;
+zeg1.forMen = true;
+zeg1.waterResistance = 10;
+
+var zeg2 = new WatchModule.Watch();
+zeg2.brand = "Fossil";
+zeg2.model = "ES4382P";
+zeg2.strapType = "Pasek skórzany";
+zeg2.price = 428;
+zeg2.forMen = false;
+zeg2.waterResistance = 5;
+
+var zeg3 = new WatchModule.Watch();
+zeg3.brand = "Diesel";
+zeg3.model = "DZ7314";
+zeg3.strapType = "Pasek skórzany";
+zeg3.price = 1198;
+zeg3.forMen = true;
+zeg3.waterResistance = 30;
+
+console.log(zeg1);
+console.log('----------------');
+
+WatchModule.addWatch(zeg1);
+WatchModule.addWatch(zeg2);
+WatchModule.addWatch(zeg3);
+
+console.log(WatchModule.findWatchesByBrand("fossil"));
+console.log('----------------');
+
+console.log(WatchModule.findWatchesByPrice(500, 1200));
+console.log('----------------');
+
+console.log(WatchModule.getFemaleWatches());
