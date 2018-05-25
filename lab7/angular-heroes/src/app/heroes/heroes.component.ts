caa66915a1d84a093c2bd197eee14e4ad37f2fe5
Kacper Dębowski <kdebowski93@gmail.com>
2018-05-25 17:08:35 +0200

    Uzupełnienie zadania 7

---
+++ lab7/angular-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../classes/hero';
+import {HEROES} from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  selectedHero: Hero;
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+}
